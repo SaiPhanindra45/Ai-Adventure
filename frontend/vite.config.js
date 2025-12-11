1227c92ca3e2c01f108b645c0967e07cd661e39b
SaiPhanindra45 <phanindrapersonal02@gmail.com>
2025-12-11 21:31:55 +0530

    add deployment instructions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+export default defineConfig((command,mode) => {
+ const env = loadEnv(mode, process.cwd(), '')
+ console.log("Environment Variables:", VITE_DEBUG);
+  return{
+     plugins: [react()],
+      server: {
+        ...(env.VITE_DEBUG== "true" && {
+          proxy: {
+            "/api": {
+              target: "http://localhost:8000",
+              changeOrigin: true,
+              secure: false,
+            }
+          }
+        })
   }
+}
 })
